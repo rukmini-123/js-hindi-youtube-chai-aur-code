3cdaff5d68cadb86ec85afd5e37a1bbaeaed3671
rukmini-123 <rukminimandal42@gmail.com>
2025-07-16 08:57:51 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,9 @@
+let accountID = 126354765
+accountID = 12
+accountEmail = "sroiwf@gmail.com"
+accountPassword = 1236453
+accountCity = "Mumbai"
+let accountState;
+
+console.log(accountID, accountEmail,accountPassword,accountCity, accountState);
+console.table([accountID, accountEmail,accountPassword,accountCity , accountState])
